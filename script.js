3b1f6e660b869331011a76694073a620babc0bf0
unknown <aolink101@gmail.com>
2022-03-05 19:50:29 -0500

    fixed minor css issues & beginning to work out timer functionality- stuck on converting milliseconds to days

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const testDay = "01 April 2022";
+const testDate = "01 April 2022";
 
-function countDown() {
+function countDown() {     
 
     const dateOfTest = new Date(testDate);
     const currentDate = new Date();
+    const seconds = ( dateOfTest - currentDate * 1000)
+    //const days = (dateOfTest - currentDate * 24000)
+    const days = (seconds / 3600 / 2)
+    console.log(dateOfTest - currentDate);
 
-    console.log(dateOfTest - currentDate)
-}
+};    
+
+// 1st Call 
 
 countDown();
+
+//setInterval(countDown, 1000);
+
