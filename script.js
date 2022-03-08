2a8457cf428fbf2add04702355ddb7261c6c378b
unknown <aolink101@gmail.com>
2022-03-08 17:28:09 -0500

    getting a wierd error in ff console Uncaught: ' TypeError: Cannot set properties of null (setting 'innerHTML') '

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+const daysE1 =document.getElementById("days");
+
+const hoursE1 =document.getElementById("hours")
+
+const minsE1 =document.getElementById("mins")
+
+const secondsE1 =document.getElementById("seconds")
+
 const testDate = "01 April 2022";
 
 function countDown() {     
 
     const dateOfTest = new Date(testDate);
     const currentDate = new Date();
-   
-      const seconds = (dateOfTest - currentDate) / 1000; /* convert to seconds */
+      const totalSeconds = (dateOfTest - currentDate) / 1000; /* convert to seconds */
+      ///console.log(Math.floor(seconds / 3600)%24);
+
          // const seconds = ( dateOfTest - currentDate * 1000)
     //const days = (dateOfTest - currentDate * 24000)
     //const days = (seconds / 3600 / 24);** Understand this Logic!
    //console.log(dateOfTest - currentDate);
-      const days = Math.floor(seconds / 3600 / 24); /*convert to / hours/ days*/
-      const hours = Math.floor(seconds / 3600) % 24
-      console.log(days)        
+      const days = Math.floor(totalSeconds / 3600 / 24); /*convert to / hours/ days*/
+      const hours = Math.floor(totalSeconds / 3600) % 24;
+      const mins = Math.floor(totalSeconds / 60) %60; 
+      //const mins = Math.floor(seconds / 60);
+    const secs = Math.floor(totalSeconds % 60);
+    
+    daysE1.innerHTML = days;
+
+    hoursE1.innerHTML = hours;
+    
+    minsE1.innerHTML = mins;
+    
+    secondsE1.innerHTML = secs;
+
 };    
 
 // 1st Call 
